760c34ab743f45fa432221ead56eacadfd8e21a8
nahidfuck <bodiastorozh@gmail.com>
2025-10-28 18:58:40 +0100

    new version

--- passwordchecker.py
+++ passwordchecker.py
@@ -1,11 +1,14 @@
 import string
+work = True
+while work:
+    password = input("Napisz swoje hasło:")
 
-def check_password(userpassword):
-    if len(userpassword) < 8:
-        print("Password is weak, try to add more characters")
-    elif any(char.isupper() for char in userpassword) and any(char.islower() for char in userpassword) and any(char.isdigit() for char in userpassword):
-        print("Password is ok, but you can add punctuation characters")
-    elif any(char.isupper() for char in userpassword) and any(char.islower() for char in userpassword) and any(string.punctuation) in userpassword:
-        print("Password is strong, good")
+    if len(password) > 11 and any(char.isupper() for char in password) and any(char.islower() for char in password) and any(char.isdigit() for char in password) and any(i in string.punctuation for i in password):
+        print("Hasło jest mocne.")
+        work = False
+    elif any(char.isupper() for char in password) and any(char.islower() for char in password) and any(char.isdigit() for char in password):
+        print("Hasło jest śriednie.")
+        work = False
+    else:
+        print("Hasło jest za słabe. Wprowadź silniejsze hasło.\nMusi być co najmniej 8 znaków, cyfr oraz wielkie i małe litery.")
 
-check_password(input("Enter your password:\n"))
